c4add68347bee0620e4a6f470112ad460335c21f
Sam Yang <samuel.q.yang@gmail.com>
2018-10-07 00:43:51 -0700

    organizers

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
  $(window).scroll(function() {
     var winTop = $(window).scrollTop();
   });
+
+$(document).ready(function(){
+	$( ".more" ).each(function(index) {
+	    $(this).on("click", function(){
+	    	var id = $(this).attr('id');
+	        // For the boolean value
+	        $("#" + id + " .showmore").slideToggle();
+	        $("#" + id + " .plus").toggleClass("rotated");
+	    });
+	});
+});
