4aac5b3567d096c568442f95263b2fac61cb71bd
Gautham Raghupathi <gauthamar11@gmail.com>
2019-08-27 23:16:38 -0700

    Update Location, oepning ceremony start time, and google maps api

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ $("#logo").click(function(event) {
         $("html, body").animate({ scrollTop: 0 }, 800);
         return false;
     });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var holberton = {lat: 37.781476, lng: -122.408119};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 12, center: holberton});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: holberton, map: map});
+}
